d64511a61bbf4bf3bdb53719a2ec64dc060037e6
fangzhipeng <fangzhipeng@duiba.com.cn>
2019-02-23 20:24:59 +0800

    init

---
+++ src/main/java/com/zhipeng/walle/test/WalleTestApplication.java
@@ -0,0 +1,13 @@
+package com.zhipeng.walle.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WalleTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WalleTestApplication.class, args);
+    }
+
+}
